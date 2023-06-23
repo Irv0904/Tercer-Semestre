fda0c2eb32b6f0a7bbea90829a17c6f5e2c81ef7
Irving Villarreal <irvingvillarreal883@gmail.com>
2023-06-22 21:49:36 -0500

    Este es Doña KEYLINIS

---
+++ 6 ejercicios/Cola.pila.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definición de la estructura de un nodo
+typedef struct Nodo {
+    int dato;
+    struct Nodo* siguiente;
+} Nodo;
+
+// Definición de la estructura de la cola
+typedef struct Lista {
+    Nodo* inicio;
+    Nodo* fin;
+} Lista;
+
+// Prototipos de funciones
+void inicializarCola(Lista* cola);
+int estaVacia(Lista cola);
+void insertarCola(Lista* cola, int dato);
+void eliminarCola(Lista* cola);
+int frenteCola(Lista cola);
+void mostrarMenu();
+
+// Función principal
+int main() {
+    Lista cola;
+    inicializarCola(&cola);
+
+    int opcion, dato;
+
+    do {
+        mostrarMenu();
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                printf("Ingrese un elemento para agregar a la cola: ");
+                scanf("%d", &dato);
+                insertarCola(&cola, dato);
+                break;
+            case 2:
+                eliminarCola(&cola);
+                break;
+            case 3:
+                printf("Frente de la cola: %d\n", frenteCola(cola));
+                break;
+            case 4:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opción inválida. Por favor, intente nuevamente.\n");
+                break;
+        }
+    } while (opcion != 4);
+
+    return 0;
+}
+
+// Inicialización de la cola
+void inicializarCola(Lista* cola) {
+    cola->inicio = NULL;
+    cola->fin = NULL;
+}
+
+// Verificar si la cola está vacía
+int estaVacia(Lista cola) {
+    return (cola.inicio == NULL);
+}
+
+// Insertar un elemento en la cola
+void insertarCola(Lista* cola, int dato) {
+    Nodo* nuevoNodo = (Nodo*)malloc(sizeof(Nodo));
+    nuevoNodo->dato = dato;
+    nuevoNodo->siguiente = NULL;
+
+    if (estaVacia(*cola)) {
+        cola->inicio = nuevoNodo;
+        cola->fin = nuevoNodo;
+    } else {
+        cola->fin->siguiente = nuevoNodo;
+        cola->fin = nuevoNodo;
+    }
+
+    printf("Elemento agregado correctamente a la cola.\n");
+}
+
+// Eliminar un elemento de la cola
+void eliminarCola(Lista* cola) {
+    if (estaVacia(*cola)) {
+        printf("La cola está vacía. No se puede eliminar ningún elemento.\n");
+    } else {
+        Nodo* nodoEliminar = cola->inicio;
+        cola->inicio = cola->inicio->siguiente;
+
+        if (cola->inicio == NULL) {
+            cola->fin = NULL;
+        }
+
+        free(nodoEliminar);
+        printf("Elemento eliminado de la cola correctamente.\n");
+    }
+}
+
+// Obtener el elemento en el frente de la cola
+int frenteCola(Lista cola) {
+    if (estaVacia(cola)) {
+        printf("La cola está vacía.\n");
+        return -1; // Valor sentinela o manejo de error
+    } else {
+        return cola.inicio->dato;
+    }
+}
+
+// Mostrar el menú de opciones
+void mostrarMenu() {
+    printf("\n--- Menú ---\n");
+    printf("1. Agregar elemento a la cola\n");
+    printf("2. Eliminar elemento de la cola\n");
+    printf("3. Mostrar frente de la cola\n");
+    printf("4. Salir\n");
+    printf("Ingrese una opción: ");
+}
---
+++ 6 ejercicios/ejercicio_16.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definición de la estructura de un nodo
+typedef struct Nodo {
+   int dato;
+   struct Nodo* siguiente;
+} Nodo;
+
+// Definición de la estructura de la cola
+typedef struct Lista {
+   Nodo* inicio;
+   Nodo* fin;
+} Lista;
+
+// Prototipos de funciones
+void inicializarCola(Lista* cola);
+int estaVacia(Lista cola);
+void insertarCola(Lista* cola, int dato);
+void eliminarCola(Lista* cola);
+void mostrarLista(Lista cola);
+void mostrarMenu();
+
+// Función principal
+int main() {
+   Lista cola;
+   inicializarCola(&cola);
+
+   int opcion, dato;
+
+   do {
+      mostrarMenu();
+      scanf("%d", &opcion);
+
+      switch (opcion) {
+            case 1:
+               printf("Ingrese un elemento para agregar a la cola: ");
+               scanf("%d", &dato);
+               insertarCola(&cola, dato);
+               system("PAUSE");
+               system("CLS");
+               break;
+            case 2:
+               eliminarCola(&cola);
+               system("PAUSE");
+               system("CLS");
+               break;
+            case 3:
+               mostrarLista(cola);
+               system("PAUSE");
+               system("CLS");
+               break;
+            case 4:
+               printf("Saliendo del programa...\n");
+               break;
+            default:
+               printf("Opción inválida. Por favor, intente nuevamente.\n");
+               break;
+      }
+   } while (opcion != 4);
+
+   return 0;
+}
+
+// Inicialización de la cola
+void inicializarCola(Lista* cola) {
+   cola->inicio = NULL;
+   cola->fin = NULL;
+}
+
+// Verificar si la cola está vacía
+int estaVacia(Lista cola) {
+   return (cola.inicio == NULL);
+}
+
+// Insertar un elemento en la cola
+void insertarCola(Lista* cola, int dato) {
+   Nodo* nuevoNodo = (Nodo*)malloc(sizeof(Nodo));
+   nuevoNodo->dato = dato;
+   nuevoNodo->siguiente = NULL;
+
+   if (estaVacia(*cola)) {
+      cola->inicio = nuevoNodo;
+      cola->fin = nuevoNodo;
+   } else {
+      cola->fin->siguiente = nuevoNodo;
+      cola->fin = nuevoNodo;
+   }
+
+   printf("Elemento agregado correctamente a la cola.\n");
+}
+
+// Eliminar un elemento de la cola
+void eliminarCola(Lista* cola) {
+   if (estaVacia(*cola)) {
+      printf("La cola está vacía. No se puede eliminar ningún elemento.\n");
+   } else {
+      Nodo* nodoEliminar = cola->inicio;
+      cola->inicio = cola->inicio->siguiente;
+
+      if (cola->inicio == NULL) {
+            cola->fin = NULL;
+      }
+      free(nodoEliminar);
+      printf("Elemento eliminado de la cola correctamente.\n");
+   }
+}
+
+// Mostrar la lista de elementos en la cola
+void mostrarLista(Lista cola) {
+   if (estaVacia(cola)) {
+      printf("La cola está vacía. No hay elementos para mostrar.\n");
+   } else {
+      Nodo* actual = cola.inicio;
+      printf("Elementos en la cola: ");
+      while (actual != NULL) {
+            printf("%d ", actual->dato);
+            actual = actual->siguiente;
+      }
+      printf("\n");
+   }
+}
+
+// Mostrar el menú de opciones
+void mostrarMenu() {
+   printf("\n Menu de Colas ---\n");
+   printf("1. Agregar \n");
+   printf("2. Eliminar \n");
+   printf("3. Mostrar \n");
+   printf("4. Salir\n");
+   printf("Ingrese una opcion: ");
+}
